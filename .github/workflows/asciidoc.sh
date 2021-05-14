e0e3e70d53699069458ed27059961cebbd0f3ceb
ledoyen <ledoyen.loic@gmail.com>
2021-05-14 09:05:04 +0200

    Fix image references

--- .github/workflows/asciidoc.sh
+++ .github/workflows/asciidoc.sh
@@ -12,8 +12,11 @@ ASCIIDOCTOR_PDF_DIR=`gem contents asciidoctor-pdf --show-install-dir`
 cp "${ASCIIDOCTOR_PDF_DIR}/data/themes/default-theme.yml" ${CURRENT_PATH}/themes/default-theme.yml
 cp -r -f "${ASCIIDOCTOR_PDF_DIR}/data/fonts/" ${CURRENT_PATH}/
 
-asciidoctor -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/html/ -o index.html  -a imagesdir=${CURRENT_PATH}/images  -r asciidoctor-diagram   index.adoc
+echo "Rendering HTML5..."
+asciidoctor -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/html/ -o index.html  -a ci=ci -a imagesdir="images"  -r asciidoctor-diagram   index.adoc
 
-asciidoctor-pdf -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/pdf/ -o index.pdf -a imagesdir=${CURRENT_PATH}/images  -a scripts@=cjk  -a pdf-styledir=${CURRENT_PATH}/themes -a pdf-fontsdir=${CURRENT_PATH}/fonts -r asciidoctor-diagram  index.adoc
+echo "Rendering PDF..."
+asciidoctor-pdf -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/pdf/ -o index.pdf -a ci=ci -a imagesdir="${CURRENT_PATH}/images"  -a scripts@=cjk  -a pdf-styledir=${CURRENT_PATH}/themes -a pdf-fontsdir=${CURRENT_PATH}/fonts -r asciidoctor-diagram  index.adoc
 
-asciidoctor-epub3 -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/epub/ -o index.epub  -r asciidoctor-diagram -a imagesdir=${CURRENT_PATH}/images   index.adoc
+echo "Rendering EPUB..."
+asciidoctor-epub3 -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/epub/ -o index.epub  -r asciidoctor-diagram -a ci=ci -a imagesdir="${CURRENT_PATH}/images"   index.adoc
