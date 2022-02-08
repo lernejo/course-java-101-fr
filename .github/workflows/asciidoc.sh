98d13e9fb66b55960f400f8ac7c15d3d901d76c8
ledoyen <ledoyen.loic@gmail.com>
2022-02-08 22:10:21 +0100

    Update rendering pipeline

--- .github/workflows/asciidoc.sh
+++ .github/workflows/asciidoc.sh
@@ -13,10 +13,10 @@ cp "${ASCIIDOCTOR_PDF_DIR}/data/themes/default-theme.yml" ${CURRENT_PATH}/themes
 cp -r -f "${ASCIIDOCTOR_PDF_DIR}/data/fonts/" ${CURRENT_PATH}/
 
 echo "Rendering HTML5..."
-asciidoctor -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/html/ -o index.html  -a ci=ci -a imagesdir="images"  -r asciidoctor-diagram   index.adoc
+asciidoctor -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/html/        -o index.html -a allow-uri-read -a ci=ci -a ci-images-dir="images"                 -a lang=fr -r asciidoctor-diagram index.adoc
 
 echo "Rendering PDF..."
-asciidoctor-pdf -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/pdf/ -o index.pdf -a ci=ci -a imagesdir="${CURRENT_PATH}/images"  -a scripts@=cjk  -a pdf-styledir=${CURRENT_PATH}/themes -a pdf-fontsdir=${CURRENT_PATH}/fonts -r asciidoctor-diagram  index.adoc
+asciidoctor-pdf -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/pdf/     -o index.pdf  -a allow-uri-read -a ci=ci -a ci-images-dir="${CURRENT_PATH}/images" -a lang=fr -r asciidoctor-diagram -a scripts@=cjk -a pdf-styledir=${CURRENT_PATH}/themes -a pdf-fontsdir=${CURRENT_PATH}/fonts index.adoc
 
 echo "Rendering EPUB..."
-asciidoctor-epub3 -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/epub/ -o index.epub  -r asciidoctor-diagram -a ci=ci -a imagesdir="${CURRENT_PATH}/images"   index.adoc
+asciidoctor-epub3 -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/epub/  -o index.epub -a allow-uri-read -a ci=ci -a ci-images-dir="${CURRENT_PATH}/images" -a lang=fr -r asciidoctor-diagram index.adoc
